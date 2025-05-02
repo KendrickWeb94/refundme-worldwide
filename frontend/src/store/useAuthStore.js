796421e920689e34846f83d27e77efa2e735814d
kendrickweb94 <kendrickzgeorge@gmail.com>
2025-05-02 13:45:10 -0800

    all

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "https://refundme-backend.onrender.com/api" : "/https://refundme-backend.onrender.com/api";
+const BASE_URL = import.meta.env.MODE === "development" ? "https://refundme-backend.onrender.com/" : "/https://refundme-backend.onrender.com/";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
